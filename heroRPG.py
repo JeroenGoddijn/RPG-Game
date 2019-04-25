49223ed200e7554a6b3944c3370c5227ce3fb662
Jeroen Goddijn <jeroen.goddijn@gmail.com>
2019-04-25 15:22:53 -0700

    Rebuild with Class inheritance for all characters

--- heroRPG.py
+++ heroRPG.py
@@ -11,72 +11,43 @@ class Character(object):
         self.type = character_type
         self.health = health
         self.power = power
-        self.healing_bonus = 0
-        self.damage_reduction = 0
-        self.power_multiplier = 1
 
     def attack(self, enemy):
-        if self.type == "hero":
-            self.power_multiplier = 0
-            if random.randint(1,5) == 3:
-                self.power_multiplier = 2
-            else:
-                self.power_multiplier = 1
-            self.healing_bonus = 0
-        # Medic can recuperate 2 health points after being attacked with a probability of 20%
-        elif self.type == "medic":
-            self.power_multiplier = 1
-            self.healing_bonus = 0
-            if random.randint(1,5) == 3:
-                self.healing_bonus = 2
-            else:
-                self.healing_bonus = 0
-        else:
-            self.healing_bonus = 0
-            self.power_multiplier = 1
-
-        enemy.health -= self.power * self.power_multiplier
-        
-        if enemy.type == "zombie":
-            print("You do {0} damage to the {1}.".format(self.power * self.power_multiplier, enemy.type))
+        if enemy.type == "deadpool":
+            print("You do {0} damage to {1}.".format(self.power, enemy.type.capitalize()))
+            enemy.health -= self.power
+            if enemy.health <= 0:
+                print("{} is dead.".format(enemy.type.capitalize()))
+        elif enemy.type == "medic":
+            if enemy.healing:
+                print(enemy.health)
+                enemy.health += 2
+                print("The {0} healed himself and recuperates 2 health points.".format(enemy.type))
+            enemy.health -= self.power
+            print("You do {0} damage to {1}.".format(self.power, enemy.type.capitalize()))
             if enemy.health <= 0:
                 print("The {} is dead.".format(enemy.type))
         elif enemy.type == "shadow":
-            if random.randint(1,10) == 5:
-                enemy.damage_reduction = self.power
-                # print("Shadow damage reduction: {}".format(enemy.damage_reduction))
+            if enemy.takes_hit:
+                enemy.health -= self.power
+                print("You do {0} damage to {1}.".format(self.power, enemy.type.capitalize()))
             else:
-                enemy.damage_reduction = 0
-                # print("Shadow damage reduction: {}".format(enemy.damage_reduction))
-            enemy.health += enemy.damage_reduction
-            print("You do {0} damage to the {1}.".format(self.power - self.damage_reduction, enemy.type))
+                print("The {0} evades your attack and you do 0 damage to {0}.".format(enemy.type))
             if enemy.health <= 0:
                 print("The {} is dead.".format(enemy.type))
-        elif enemy.type == "medic":
-            # print(enemy.healing_bonus)
-            if enemy.healing_bonus != 0:
-                print("The {0} healed himself and recuperates {1} health points.".format(enemy.type, enemy.healing_bonus))
-                enemy.health += enemy.healing_bonus
-            else:
-                enemy.health += enemy.healing_bonus       
-            print("You do {0} damage to the {1}.".format(self.power * self.power_multiplier, enemy.type))
-            if enemy.health <= 0:
-                print("The {} is dead.".format(enemy.type))
-        elif enemy.type == "deadpool":
-            print("You do {0} damage to {1}.".format(self.power * self.power_multiplier, enemy.type.capitalize()))
-            if enemy.health <= 0:
-                print("{} is dead.".format(enemy.type.capitalize()))
         elif enemy.type != "hero":
-            print("You do {0} damage to the {1}.".format(self.power * self.power_multiplier, enemy.type))
+            enemy.health -= self.power
+            print("You do {0} damage to the {1}.".format(self.power, enemy.type))
             if enemy.health <= 0:
                 print("The {} is dead.".format(enemy.type))
         else:
             if self.type == "hulk":
-                print("{0} SMASH!!! and does {1} damage to you.".format(self.type.upper(), self.power - enemy.damage_reduction))
+                print("{0} SMASH!!! and does {1} damage to you.".format(self.type.upper(), self.power))
             elif self.type == "deadpool":
-                print("{0} does {1} damage to you.".format(self.type.capitalize(), self.power - enemy.damage_reduction))
+                print("{0} does {1} damage to you.".format(self.type.capitalize(), self.power))
             else:
-                print("The {0} does {1} damage to you.".format(self.type, self.power - enemy.damage_reduction))
+                print("The {0} does {1} damage to you.".format(self.type, self.power))
+            enemy.health -= self.power
             if enemy.health <= 0:
                 print("You are dead.")        
     
@@ -97,14 +68,55 @@ class Character(object):
             else:
                 print("The {} has {} health and {} power.".format(self.type, self.health, self.power))
 
+class Goblin(Character):
+    def __init__(self, character_type, health, power):
+        Character.__init__(self, character_type, health, power)
+
+class Medic(Character):
+    def __init__(self, character_type, health, power):
+        Character.__init__(self, character_type, health, power)
+        if random.randint(1,5) == 3:
+            self.healing = True
+        else:
+            self.healing = False
+
+class Zombie(Character):
+    def __init__(self, character_type, health, power):
+        Character.__init__(self, character_type, health, power)
+        self.damage_reduction = 0
+
+class Shadow(Character):
+    def __init__(self, character_type, health, power):
+        Character.__init__(self, character_type, health, power)
+
+        if random.randint(1,10) == 5:
+            self.takes_hit = True
+        else:
+            self.takes_hit = False
+
+class Hulk(Character):
+    def __init__(self, character_type, health, power):
+        Character.__init__(self, character_type, health, power)
+
+class Deadpool(Character):
+    def __init__(self, character_type, health, power):
+        Character.__init__(self, character_type, health, power)
+
+class Hero(Character):
+    def __init__(self, character_type, health, power):
+        Character.__init__(self, character_type, health, power)
+        if random.randint(1,5) == 3:
+            self.power = power* 2
+
+
 def main():
-    hero = Character("hero", 10, 5)
-    goblin = Character("goblin", 6, 2)
-    zombie = Character("zombie", float("inf"), 3)
-    medic = Character("medic", 9, 3)
-    shadow = Character("shadow", 20, 1)
-    hulk = Character("hulk", 50, 10)
-    deadpool = Character("deadpool", 15, 4)
+    hero = Hero("hero", 10, 5)
+    goblin = Goblin("goblin", 6, 2)
+    zombie = Zombie("zombie", float("inf"), 3)
+    medic = Medic("medic", 9, 3)
+    shadow = Shadow("shadow", 20, 1)
+    hulk = Hulk("hulk", 50, 10)
+    deadpool = Deadpool("deadpool", 15, 4)
 
     opponent = ""
     random_opponent = random.randint(1,6)
